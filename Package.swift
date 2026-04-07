d75c1f112e7073c95c94f9c18caadc6609c97c56
thrivinghari <172593625+thrivinghari@users.noreply.github.com>
2026-04-07 10:24:17 +0530

    update deps - modelhike from the main branch in remote git

--- Package.swift
+++ Package.swift
@@ -14,8 +14,7 @@ let package = Package(
         .executable(name: "DevTester_Blueprints", targets: ["DevTester_Blueprints"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/modelhike/modelhike", from: "0.2.1"),
-        //.package(path: "../modelhike")
+        .package(url: "https://github.com/modelhike/modelhike", branch: "main")
     ],
     targets: [
         .target(
